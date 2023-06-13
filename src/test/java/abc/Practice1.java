5562a972cc5a1f56e94be81cc77e4524be476996
mohangowdak <mohan.k@testyantra.com>
2023-06-13 16:57:45 +0530

    FirstCommit

---
+++ src/test/java/abc/Practice1.java
@@ -0,0 +1,5 @@
+package abc;
+
+public class Practice1 {
+//engg1
+}
---
+++ src/test/java/abc/Practice2.java
@@ -0,0 +1,11 @@
+package abc;
+
+public class Practice2 {
+public static void main(String[] args) {
+//abc 
+  //commit1
+  //commit2
+  //commit3
+
+}
+}
